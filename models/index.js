2ae075bc10c0102074dc051266804579d7362eb5
Melissa Cadena <melissabcadena@Melissas-MBP.attlocal.net>
2020-08-16 16:42:40 -0500

    testing associations

--- models/index.js
+++ models/index.js
@@ -13,13 +13,13 @@ Category.hasMany(Product);
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag, 
-  as: 'product_id'
+  foreignKey: 'product_id'
 })
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag, 
-  as: 'tag_id'
+  foreignKey: 'tag_id'
 })
 
 module.exports = {
