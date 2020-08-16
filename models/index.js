ac6aad6f7bf5ec9a8c759907566b53e27365c357
Melissa Cadena <melissabcadena@Melissas-MBP.attlocal.net>
2020-08-16 16:01:54 -0500

    add associations

--- models/index.js
+++ models/index.js
@@ -5,12 +5,22 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category)
 
 // Categories have many Products
+Category.hasMany(Product);
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag, 
+  as: 'product_id'
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag, 
+  as: 'tag_id'
+})
 
 module.exports = {
   Product,
